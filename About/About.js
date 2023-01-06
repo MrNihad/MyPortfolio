b62b4315b5be70b8348c5216d5a632dc7ccf6ece
Nihad Qurbanov <64197660+MrNihad@users.noreply.github.com>
2023-01-06 02:40:01 +0100

    trying to fix nav bar on about page

--- About/About.js
+++ About/About.js
@@ -73,15 +73,7 @@ $(document).ready(function() {
     });
     
   
-    /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-  function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
+    
   });
   // select the navbar element
   var navbar = document.querySelector('.topnav');
@@ -108,4 +100,24 @@ $(document).ready(function() {
     // update the current scroll position
     scrollPos = currentScrollPos;
   });
+  /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+  function myFunction() {
+    // Log a message to the console to indicate that the function has been called
+    console.log("myFunction called");
+
+    // Select the topnav element
+    var x = document.getElementById("myTopnav");
+
+    // Log the value of the className property to the console before toggling the class
+    console.log("className before toggle: " + x.className);
+
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+    // Log the value of the className property to the console after toggling the class
+    console.log("className after toggle: " + x.className);
+
+  }
   
--- Home.js
+++ Home.js
@@ -1,9 +1,20 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  // Log a message to the console to indicate that the function has been called
+  console.log("myFunction called");
+
+  // Select the topnav element
+  var x = document.getElementById("myTopnav");
+
+  // Log the value of the className property to the console before toggling the class
+  console.log("className before toggle: " + x.className);
+
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+  // Log the value of the className property to the console after toggling the class
+  console.log("className after toggle: " + x.className);
+
+}
